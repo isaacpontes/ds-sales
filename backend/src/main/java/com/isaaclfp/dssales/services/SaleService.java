66249fa6ec790b87a99d036c60c2f3794bff9c59
Isaac L. F. Pontes <isaaclfp@outlook.com>
2021-05-08 12:35:07 -0300

    Added sales endpoint with pagination.

---
+++ backend/src/main/java/com/isaaclfp/dssales/services/SaleService.java
@@ -0,0 +1,29 @@
+package com.isaaclfp.dssales.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.isaaclfp.dssales.dto.SaleDTO;
+import com.isaaclfp.dssales.entities.Sale;
+import com.isaaclfp.dssales.repositories.SaleRepository;
+import com.isaaclfp.dssales.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
