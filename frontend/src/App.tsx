6ee8dbc662deb88857c0fa4d941e85a58d94ba58
Isaac L. F. Pontes <isaaclfp@outlook.com>
2021-05-09 19:13:48 -0300

    Added routes support.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,9 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
-import Navbar from "components/Navbar";
+import Routes from "Routes";
 
 export default function App() {
   return (
     <>
-      <Navbar />
-      <div className="container py-3">
-        <h1 className="text-primary py-3">Sales Dashboard</h1>
-
-        <div className="row p-3">
-          <div className="col-sm-6">
-            <h5 className="text-secondary text-center">Conversion Rate (%)</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-secondary text-center">All Sales</h5>
-            <DonutChart />
-          </div>
-        </div>
-
-        <div className="py-5">
-          <h2 className="text-primary py-3">All Sales</h2>
-          <DataTable />
-        </div>
-      </div>
-      <Footer />
+      <Routes />
     </>
   );
 }
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,33 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
+
+export default function Dashboard() {
+    return (
+        <>
+            <Navbar />
+            <div className="container py-3">
+                <h1 className="text-primary py-3">Sales Dashboard</h1>
+
+                <div className="row p-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-secondary text-center">Conversion Rate (%)</h5>
+                        <BarChart />
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-secondary text-center">All Sales</h5>
+                        <DonutChart />
+                    </div>
+                </div>
+
+                <div className="py-5">
+                    <h2 className="text-primary py-3">All Sales</h2>
+                    <DataTable />
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+}
